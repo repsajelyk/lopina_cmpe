16efd4839510609be569ec6547ff32bd88d05e3d
repsajelyk <jasperkyle31@gmail.com>
2025-10-18 14:17:28 +0800

    ACT

---
+++ WEEK4/WEEK4.1.py
@@ -0,0 +1,42 @@
+print("hello world")
+print("sabi ko hello world")
+input("Enter your name: ")
+print("angas naman niyan sah")
+input("Enter your age: ")
+print("gurang")
+input("Enter your height: ")
+print("tangkad")
+
+input("How do you feel? ")
+print("nyek hahaha")
+
+#import serial
+
+
+myString = "HELLO WORLD"
+myAge = 18.302
+myHeight = 5,4
+
+print("   ")
+print(myString)
+print(myAge)
+print(myHeight)
+
+combinemyStringAndmyAge = "HELLO WORLD" + "  " + str(18.302)
+
+print(combinemyStringAndmyAge)
+
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+c=input("Enter third number: ")
+
+sumString = a+b+c
+sumInteger = int(a) + int(b) + int(c)
+
+print(sumInteger)
+
+strFullname = "jasper"
+intlength = jas(strFullname)
+print(intlength)
+print(strFullname[3])
+print(strFullname[5])
